9e8d3b0d6500319b4504bd0375be9878665f2a3d
ryan <eldridger@wit.edu>
2013-08-03 23:33:20 -0400

    FORGOT DA FILE

---
+++ xxx/scripts/moarTesting.php
@@ -0,0 +1,22 @@
+<?php
+
+$UN = $_POST["uname"];
+$PW = $_POST["pw"];
+$msg;
+
+$connect = mysql_connect("localhost:3306", "root", "dba");
+//echo($connect);
+
+@mysql_select_db(testingthings) or die("Unable to connect to the database");
+
+$query = mysql_query("SELECT `Password` FROM `testingthings`.`login` WHERE `Username` = '$UN'") or die('Username not found');
+
+$row = mysql_fetch_row($query);
+if ($row[0] == $PW) {
+	echo 'success';
+} else {
+	echo 'incorrect password';
+}
+
+
+?>
